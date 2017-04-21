d902f2fa9d1a2836b091a8a1a9593af30aba2048
cofdev0 <cofdev0@hh2013.33mail.com>
2017-04-21 07:48:17 -0500

    =missing contract

--- contracts/EntboxContract.sol
+++ contracts/EntboxContract.sol
@@ -16,7 +16,9 @@ function transferOwnership(address newOwner) onlyOwner {
 }
 }
 
-contract StandardToken { mapping( address => uint ) _balances; uint public _supply; uint256 public totalSupply;
+contract StandardToken { mapping( address => uint ) _balances;
+uint public _supply;
+uint256 public totalSupply;
 
 event Transfer(address indexed from, address indexed to, uint value);
 
@@ -43,41 +45,93 @@ return true;
 
 function safeToAdd(uint a, uint b) internal returns (bool) {
 return (a + b >= a);
-} }
-
-contract ChainsOfFreedomToken is StandardToken, Owned { string public constant name = "Chains Of Freedom"; string public constant symbol = "DT"; uint public constant decimals = 0; string public constant version = "0.1"; uint256 public totalSupply = 0; uint256 public credit = 0;
-
-mapping( address => uint ) _balances;
-
-event Buy(address indexed sender, uint eth, uint fbt);
-
-function ChainsOfFreedomToken(
-	uint initialBalance,
-	address centralMinter
-	) {
-	if(centralMinter != 0 ) owner = msg.sender;
-	_balances[msg.sender] = initialBalance;
-	initialSupply = initialBalance;
 }
 
-function transfer(address _to, uint256 _value) returns (bool success) {
-	return super.transfer(_to, _value);
 }
 
-function buyToken(address target, uint256 amount) onlyOwner {
-    
-    mintToken(target, amount);
+
+contract EntboxContract is StandardToken, Owned {
+string public constant name = "Chains Of Freedom";
+string public constant symbol = "Dets";
+uint public constant decimals = 0;
+string public constant version = "0.1";
+
+struct DetsReceipt {
+    string id;
+    address tokenCreator;
+    uint detsAmount;
+    uint euroAmount;
+    bool tokenCreatedStatus;
 }
 
-function mintToken(address target, uint256 mintedAmount) onlyOwner {
-	_balances[target] += mintedAmount;
-	totalSupply += mintedAmount;
-	Transfer(0, owner, mintedAmount);
-	Transfer(owner, target, mintedAmount);
+struct DetsDestruction {
+    string id;
+    address destroyer;
+    uint detsDestroyed;
+    string iban;
 }
 
-function() {
-	throw;
+mapping( string => DetsReceipt ) receipts;
+mapping( string => DetsDestruction ) destructions;
+mapping( address => uint ) balances;
+uint256 totalDets = 0;
+
+function EntboxContract() {
+	owner = msg.sender;
 }
+
+function getTotalDetsAmount() returns (uint256 detsAmount) {
+    return totalDets;
+}
+
+function storeReceipt(string id, address tokenCreator, uint detsAmount, uint euroAmount) onlyOwner {
+    receipts[id] = DetsReceipt(id, tokenCreator, detsAmount, euroAmount,false);
+}
+function getDetsAmountFromReceipt(string id) returns (uint detsAmount) {
+    return receipts[id].detsAmount;
+}
+function getEuroAmountFromReceipt(string id) returns (uint euroAmount) {
+    return receipts[id].euroAmount;
+}
+function getTokenCreatorFromReceipt(string id) returns (address tokenCreator) {
+    return receipts[id].tokenCreator;
+}
+function getTokenCreatedStatusFromReceipt(string id) returns (bool tokenCreatedStatus) {
+    return receipts[id].tokenCreatedStatus;
+}
+
+function createDets(string id) {
+    if(receipts[id].tokenCreatedStatus) throw;
+    if(getDetsAmountFromReceipt(id) <= 0) throw;
+    receipts[id].tokenCreatedStatus = true;
+    var addr = getTokenCreatorFromReceipt(id);
+    var detsToCreate = getDetsAmountFromReceipt(id);
+    balances[addr] += detsToCreate;
+    totalSupply += detsToCreate;
+}
+
+function getDetsBalance(address addr) returns (uint detsAmount) {
+    return balances[addr];
+}
+
+function destroyDets(string id, uint detsToDestroy, string iban) {
+    if(balances[msg.sender] < detsToDestroy) throw;
+    if(getDetsDestroyed(id)!=0) throw;
+    if(detsToDestroy==0) throw;
+    balances[msg.sender] -= detsToDestroy;
+    totalDets -= detsToDestroy;
+    destructions[id] = DetsDestruction(id, msg.sender, detsToDestroy, iban);
+}
+
+function getDetsDestroyer(string id) returns (address detsDestroyer) {
+    return destructions[id].destroyer;
+}
+function getDetsDestroyed(string id) returns (uint detsDestroyed) {
+    return destructions[id].detsDestroyed;
+}
+function getIban(string id) returns (string iban) {
+    return destructions[id].iban;
+}
+
 }
 
